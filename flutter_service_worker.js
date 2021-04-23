e1263252848857183e0c3a65a04e2a8bc701f74b
Abin Tom <abintom@outlook.com>
2021-04-23 20:51:41 +0530

    Add option to input decimal value for product prices

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "c68cf2ce3bd59d406d031eb3c92d8015",
-"index.html": "973e8ba06d84203d88cccad8e7602022",
-"/": "973e8ba06d84203d88cccad8e7602022",
-"main.dart.js": "f559a4360ecb064fe3191a61e5a5ab81",
+"index.html": "3c07d5bb6ad4ca77126d7dc7612bb396",
+"/": "3c07d5bb6ad4ca77126d7dc7612bb396",
+"main.dart.js": "a066d688657cd5ef1049ff9b9362cb24",
 "favicon.png": "98a8a987604f462755643b8412c7e92e",
 "icons/Icon-192.png": "af9033555c6feb8da9491d65ab5bf48f",
 "icons/Icon-128.png": "204204d5ae02c85b295d363f3bbe4294",
